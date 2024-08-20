3daff4851eddb7378ecfb5d86b04de318cd07c62
Shun-ichi Tahara <jado@flowernet.gr.jp>
2024-08-21 06:34:56 +0900

    Fix error messages

--- web/lib/password_controller.rb
+++ web/lib/password_controller.rb
@@ -10,19 +10,19 @@ class PasswordController < Sinatra::Base
     user = get_user(:or_login)
 
     unless user.authenticate(params[:password])
-      @message = _('Invalid current password', :passwd)
+      @message = _('Invalid current password.', :passwd)
       return view(:passwd)
     end
 
     unless params[:new_password] == params[:new_password_verify]
-      @message = _('New passwords unmatch', :passwd)
+      @message = _('New passwords unmatch.', :passwd)
       return view(:passwd)
     end
 
     user.password = params[:new_password]
     user.password_confirmation = params[:new_password_verify]
     unless user.save
-      @message = _('Changing password failed', :passwd)
+      @message = _('Changing password failed.', :passwd)
       return view(:passwd)
     end
 
