b0c9cc73aad63ff9f4b91aed1417a16065b1d516
Shun-ichi Tahara <jado@flowernet.gr.jp>
2026-01-31 22:16:52 +0900

    Fix problem on redefining accounts again

---
+++ web/db/migrate/20260130131546_add_cond_to_index_on_users.rb
@@ -0,0 +1,6 @@
+class AddCondToIndexOnUsers < ActiveRecord::Migration[6.1]
+  def change
+    remove_index :users, column: :name, unique: true
+    add_index :users, :name, unique: true, where: 'invalid_flag = false'
+  end
+end
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_19_000519) do
+ActiveRecord::Schema.define(version: 2026_01_30_131546) do
 
   create_table "users", force: :cascade do |t|
     t.text "name", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2024_07_19_000519) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["invalid_flag"], name: "index_users_on_invalid_flag"
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true, where: "invalid_flag = false"
   end
 
 end
